2e2326d05b9365d5f649b399b7167fdf0a9cea2f
housebakshi <bakshithedeveloper@gmail.com>
2020-11-23 18:05:35 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React from "react"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      button_text: "Hide Image",
+      is_image_hidden: false
+    }
+  }
+  
+  toggle_image(){
+    const {state} = this;
+    state.is_image_hidden  = !state.is_image_hidden;
+    if(!state.is_image_hidden){
+      state.button_text = "Hide Image";
+    }else {
+      state.button_text = "Display Image"
+    }
+
+    this.setState({
+      ...state
+    })
+  }
+  render(){
+    const {button_text , is_image_hidden} = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          {!is_image_hidden && (<img src={logo} className="App-logo" alt="logo" />)}
+          
+          <p>
+            Click Button To Toggle The Image
+          </p>
+          <button onClick={() => this.toggle_image()}>
+            {button_text}
+          </button>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
